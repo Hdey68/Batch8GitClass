6934015cbfecefede34c386941264f480e16c8d4
Hdey68 <ouniche.sofiane@gmail.com>
2021-01-25 18:22:17 -0500

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println ("hello batch8" );
+        System.out.println ("second commit" );
     }
 }
